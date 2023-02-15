453f21cb3495b31cc5f552e6ee2d67a6f42405d2
VADT77 <turanosoff@gmail.com>
2023-02-15 23:03:02 +0300

    "json.changes"

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -19,6 +19,17 @@ class MobileBankApiTestV1 {
                 // Проверки
                 .then()
                 .statusCode(200);
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .statusCode(200)
+                .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
     }
 }
 
